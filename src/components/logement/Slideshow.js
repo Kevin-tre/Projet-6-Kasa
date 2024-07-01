b9e6d9d2c7d99036841fd7edbdd33944c11410ca
Kevin-tre <kevintrepied@gmail.com>
2024-07-02 01:51:30 +0200

    Add slideshow

---
+++ src/components/logement/Slideshow.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import ArrowLeft from "../../assets/images/arrowleft.png";
+import ArrowRight from "../../assets/images/arrowright.png";
+import "./slideshow.scss";
+
+const Slideshow = ({ pictures, title }) => {
+  const [slide, setSlide] = useState(0);
+  console.log(title);
+
+  const nextSlide = () => {
+    setSlide(slide === pictures.length - 1 ? 0 : slide + 1);
+  };
+
+  const prevSlide = () => {
+    setSlide(slide === 0 ? pictures.length - 1 : slide - 1);
+  };
+
+  return (
+    <div className="carousel">
+      <img
+        onClick={nextSlide}
+        src={ArrowRight}
+        alt="flèche droite"
+        className=" arrow arrow-right"
+      />
+      {pictures.map((pictures, index) => {
+        return (
+          <img
+            src={pictures}
+            alt={title}
+            key={index}
+            className={slide === index ? "slide" : "slide slide-hidden"}
+          />
+        );
+      })}
+      <img
+        onClick={prevSlide}
+        src={ArrowLeft}
+        alt="flèche gauche"
+        className=" arrow arrow-left"
+      />
+      <div className="slide-numbers">
+        <span>
+          {slide + 1} / {pictures.length}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Slideshow;
--- src/pages/logement/Logement.js
+++ src/pages/logement/Logement.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Collapse from "../../components/Collapse";
+import HostInfo from "../../components/logement/HostInfos";
+import Rating from "../../components/logement/Rating";
+import Slideshow from "../../components/logement/Slideshow";
+import Tag from "../../components/logement/Tag";
+import logementsData from "../../data/logements.json";
+import "./logement.scss";
 
 const Logement = () => {
-  const { id } = useParams();
-  return <div>{id}</div>;
+  const { id } = useParams(); // Extraire l'ID de l'URL
+  const [logement, setLogement] = useState(null); // Initialiser l'état du logement
+
+  useEffect(() => {
+    // Trouver le logement correspondant à l'ID dans les données
+    const foundLogement = logementsData.find((logement) => logement.id === id);
+    // Mettre à jour l'état du logement
+    setLogement(foundLogement);
+  }, [id]); // Execute cet effet lorsque l'ID change
+
+  if (!logement) {
+    return <div>Chargement...</div>;
+  }
+
+  return (
+    <div className="logement-container">
+      <Slideshow pictures={logement.pictures} title={logement.title} />
+      <div className="title-host">
+        <div>
+          <h1>{logement.title}</h1>
+          <p className="title-host-location">{logement.location}</p>
+        </div>
+        <HostInfo host={logement.host} />
+      </div>
+      <div className="tag-rating">
+        <Tag tags={logement.tags} />
+        <Rating rating={logement.rating} />
+      </div>
+      <div className="logement-collapse-container">
+        <div className="logement-collapse">
+          <Collapse title="Description" content={logement.description} />
+          <Collapse title="Équipements" content={logement.equipments} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Logement;
